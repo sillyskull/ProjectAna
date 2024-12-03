e74bb4b8b4ae6a8a8d3b972a0d606182045f0431
sillyskull <rohan5144320@gmail.com>
2024-12-04 01:40:19 +0530

    removed unsed import in main.tsx

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"; // Correct module
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LoginCardWithForm } from "@/components/login-form";
-import { CaseDataTable } from "@/components/case-data-table";
-import { DataFileTable } from "@/components/data-files-table";
+
 
 const container = document.getElementById("root");
 if (!container) {
